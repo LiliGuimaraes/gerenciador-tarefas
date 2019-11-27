177ea8ee5daaeb697a15d785775db7a195ac8e01
Lilian Guimarães <lilian.guimaraes.silva@gmail.com>
2019-11-27 20:23:14 -0300

    add inumerous things

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from flask import request
-
+from operator import itemgetter
 
 app = FastAPI()
 
@@ -27,3 +27,83 @@ def criar():
         'estado': False
     }
     return jsonify(tarefa)
+
+@app.route('/task')
+def listar():
+    return jsonify(sorted(tarefas, key=itemgetter('estado')))
+
+@app.route('/task/<int:id_tarefa>', methods=['DELETE'])
+def remover(id_tarefa):
+    return ''
+
+@app.route('/task/<int:id_tarefa>', methods=['DELETE'])
+def remover(id_tarefa):
+    return '', 204
+
+@app.route('/task/<int:id_tarefa>', methods=['DELETE'])
+def remover(id_tarefa):
+    tarefa = [tarefa for tarefa in tarefas if tarefa['id'] == id_tarefa]
+    tarefas.remove(tarefa[0])
+    return '', 204
+
+@app.route('/task/<int:id_tarefa>', methods=['DELETE'])
+def remover(id_tarefa):
+    tarefa = [tarefa for tarefa in tarefas if tarefa['id'] == id_tarefa]
+    if not tarefa:
+        abort(404)
+    tarefas.remove(tarefa[0])
+    return '', 204
+
+@app.route('/task/<int:id_tarefa>', methods=['GET'])
+def detalhar(id_tarefa):
+    tarefa = [tarefa for tarefa in tarefas if tarefa['id'] == id_tarefa]
+    return jsonify(tarefa[0])
+
+@app.route('/task/<int:id_tarefa>', methods=['GET'])
+def detalhar(id_tarefa):
+    tarefa = [tarefa for tarefa in tarefas if tarefa['id'] == id_tarefa]
+    if not tarefa:
+        abort(404)
+    return jsonify(tarefa[0])
+
+@app.route('/tarefa/<int:id_tarefa>', methods=['PUT'])
+def atualizar(id_tarefa):
+    tarefa = [tarefa for tarefa in tarefas if tarefa['id'] == id_tarefa]
+    titulo = request.json.get('titulo')
+    descricao = request.json.get('descricao')
+    estado = request.json.get('estado')
+    tarefa_escolhida = tarefa[0]
+    tarefa_escolhida['titulo'] = titulo or tarefa_escolhida['titulo']
+    tarefa_escolhida['descricao'] = descricao or tarefa_escolhida['descricao']
+    tarefa_escolhida['estado'] = estado or tarefa_escolhida['estado']
+    return jsonify(tarefa_escolhida)
+
+@app.route('/tarefa/<int:id_tarefa>', methods=['PUT'])
+def atualizar(id_tarefa):
+    tarefa = [tarefa for tarefa in tarefas if tarefa['id'] == id_tarefa]
+    titulo = request.json.get('titulo')
+    descricao = request.json.get('descricao')
+    entregue = request.json.get('entregue')
+    if not tarefa:
+        abort(404)
+    tarefa_escolhida = tarefa[0]
+    tarefa_escolhida['titulo'] = titulo or tarefa_escolhida['titulo']
+    tarefa_escolhida['descricao'] = descricao or tarefa_escolhida['descricao']
+    tarefa_escolhida['entregue'] = entregue or tarefa_escolhida['entregue']
+    return jsonify(tarefa_escolhida)
+
+@app.route('/tarefa/<int:id_tarefa>', methods=['PUT'])
+def atualizar(id_tarefa):
+    tarefa = [tarefa for tarefa in tarefas if tarefa['id'] == id_tarefa]
+    titulo = request.json.get('titulo')
+    descricao = request.json.get('descricao')
+    estado = request.json.get('estado')
+    if not tarefa:
+        abort(404)
+    if not descricao or not titulo or estado is None:
+        abort(400)
+    tarefa_escolhida = tarefa[0]
+    tarefa_escolhida['titulo'] = titulo or tarefa_escolhida['titulo']
+    tarefa_escolhida['descricao'] = descricao or tarefa_escolhida['descricao']
+    tarefa_escolhida['estado'] = estado or tarefa_escolhida['estado']
+    return jsonify(tarefa_escolhida)
--- tests/tests_gerenciador.py
+++ tests/tests_gerenciador.py
@@ -90,3 +90,109 @@ def test_criar_tarefa_sem_descricao():
     resposta = cliente.post('/task', data=json.dumps({'titulo': 'titulo'}),
                             content_type='application/json')
     assert resposta.status_code == 400
+
+def test_criar_tarefa_sem_titulo():
+    cliente = app.test_client()
+    # o código de status deve ser 400 indicando um erro do cliente
+    resposta = cliente.post('/task', data=json.dumps(
+        {'descricao': 'descricao'}),
+        content_type='application/json')
+    assert resposta.status_code == 400
+
+def test_listar_tarefas_deve_apresentar_tarefas_nao_finalizadas_primeiro():
+    tarefas.clear()
+    tarefas.append({'id': 1, 'titulo': 'tarefa 1', 'descricao': 'tarefa de numero 1',
+                    'estado': True})
+    tarefas.append({'id': 2, 'titulo': 'tarefa 2', 'descricao': 'tarefa de numero 2',
+                    'estado': False})
+    with app.test_client() as cliente:
+        resposta = cliente.get('/task')
+        data = json.loads(resposta.data.decode('utf-8'))
+        primeira_task, segunda_task = data
+        assert primeira_task['titulo'] == 'tarefa 2'
+        assert segunda_task['titulo'] == 'tarefa 1'
+
+def test_deletar_tarefa_utiliza_verbo_delete():
+    tarefas.clear()
+    with app.test_client() as cliente:
+        resposta = cliente.delete('/task/1')
+        assert resposta.status_code != 405
+
+def test_remover_tarefa_existente_retorna_204():
+    tarefas.clear()
+    tarefas.append({'id': 1, 'titulo': 'titulo',
+                    'descricao': 'descricao', 'estado': False})
+    cliente = app.test_client()
+    resposta = cliente.delete('/task/1', content_type='application/json')
+    assert resposta.status_code == 204
+    assert resposta.data == b''
+
+def test_remover_tarefa_existente_remove_tarefa_da_lista():
+    tarefas.clear()
+    tarefas.append({'id': 1, 'titulo': 'titulo',
+                    'descricao': 'descricao', 'estado': False})
+    cliente = app.test_client()
+    cliente.delete('/task/1', content_type='application/json')
+    assert len(tarefas) == 0
+
+def test_remover_tarefa_nao_existente():
+    tarefas.clear()
+    cliente = app.test_client()
+    resposta = cliente.delete('/task/1', content_type='application/json')
+    assert resposta.status_code == 404
+
+def test_detalhar_tarefa_existente():
+    tarefas.clear()
+    tarefas.append({'id': 1, 'titulo': 'titulo',
+                    'descricao': 'descricao', 'entregue': False})
+    cliente = app.test_client()
+    resposta = cliente.get('/task/1', content_type='application/json')
+    data = json.loads(resposta.data.decode('utf-8'))
+    assert resposta.status_code == 200
+    assert data['id'] == 1
+    assert data['titulo'] == 'titulo'
+    assert data['descricao'] == 'descricao'
+    assert data['entregue'] is False
+
+def test_detalhar_tarefa_nao_existente():
+    tarefas.clear()
+    cliente = app.test_client()
+    resposta = cliente.get('/task/1', content_type='application/json')
+    assert resposta.status_code == 404
+
+def test_atualizando_uma_tarefa_nao_existente():
+    tarefas.clear()
+    cliente = app.test_client()
+    resposta = cliente.put('/tarefa/1', data=json.dumps(
+        {'titulo': 'titulo atualizado',
+         'decricao': 'descricao atualizada', 'estado': True}
+    ),
+        content_type='application/json')
+    assert resposta.status_code == 404
+
+def test_atualizando_uma_tarefa_com_campos_invalidos():
+    tarefas.clear()
+    tarefas.append({'id': 1, 'titulo': 'titulo',
+                    'descricao': 'descricao', 'estado': False})
+    cliente = app.test_client()
+    # sem estado
+    resposta = cliente.put('/tarefa/1', data=json.dumps(
+        {'titulo': 'titulo atualizado',
+         'decricao': 'descricao atualizada'}
+    ),
+        content_type='application/json')
+    assert resposta.status_code == 400
+    # sem descrição
+    resposta = cliente.put('/tarefa/1', data=json.dumps(
+        {'titulo': 'titulo atualizado',
+         'estado': False}
+    ),
+        content_type='application/json')
+    assert resposta.status_code == 400
+    # sem titulo
+    resposta = cliente.put('/tarefa/1', data=json.dumps(
+        {'descricao': 'descricao atualizado',
+         'estado': False}
+    ),
+        content_type='application/json')
+    assert resposta.status_code == 400
